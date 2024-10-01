dad3b102c3795d799e0077b203b40d9bb40587e4
Tushar Sahani <104075614+Tushar-sahani@users.noreply.github.com>
2024-10-01 22:02:29 +0530

    Delete middleware directory

--- middleware/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-
-const app = require('./app.js');
-const PORT = process.env.PORT || 5000;
-require("dotenv").config();
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
